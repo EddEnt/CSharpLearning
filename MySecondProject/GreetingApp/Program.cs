aebe19b826488e67b352147d96391860ad6444c6
EddEnt <61289536+EddEnt@users.noreply.github.com>
2023-06-26 11:46:42 -0400

    String variables & relevant information.

---
+++ MySecondProject/GreetingApp/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+//Welcome user to the app
+Console.WriteLine("Welcome to the Greeting App, User!");
+Console.WriteLine("This app was built by Edward Entecott.");
+Console.WriteLine("------------------------------------------");
+Console.WriteLine();
+
+//Ask user for their first name
+Console.Write("To start, please enter your first name: ");
+
+string? firstName; //? denotes a string as potentially being null
+firstName = Console.ReadLine();
+
+//Output greeting user by their first name
+Console.WriteLine("Well done " + firstName + ", now you know about capturing user input into variables. :) ");
+Console.WriteLine("Thank you for using the application. The app will now exit on a key press.");
+Console.ReadKey();
+
---
+++ StringVariablesApp/StringVariablesApp/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+string firstName = string.Empty; //Industry standard for a string, initialises an empty string, "" is similar, but is a string without any content inside
+string lastName = string.Empty;
+string filePath = string.Empty;
+
+firstName = "Edward"; //Assigning a value to the string
+lastName = "Entecott";
+//filePath = "C:\Temp\Demo"; //Wont work like this, \ are for escape characters
+//filePath = "C:\\Temp\\Demo"; //A second \ will add a \ to the string, will look like one \ when printed
+filePath = @"C:\Temp\Demo"; //@ is a string literal character, great to use when not using any escape characters like \n <-(new line)
+
+//Console.WriteLine(firstName); //Using a string
+//Console.WriteLine(firstName + " " + lastName ); //Appending strings
+//Console.WriteLine($"{firstName} {lastName}"); //String interpolation, treat whatever is in the curly braces as C# code
+Console.WriteLine($"Hello {firstName} {lastName}"); //String interpolation, treat whatever is in the curly braces as C# code
+Console.WriteLine(filePath);
+
+
+
