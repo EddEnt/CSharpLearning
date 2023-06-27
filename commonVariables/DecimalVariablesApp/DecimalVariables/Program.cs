069afa5666d89eaad397c3e3cec99484083230bf
EddEnt <61289536+EddEnt@users.noreply.github.com>
2023-06-26 21:58:39 -0400

    Decimal work commit

---
+++ commonVariables/DecimalVariablesApp/DecimalVariables/Program.cs
@@ -0,0 +1,9 @@
+﻿
+//Mainly used in money or very, very accurate systems (NASA, space based systems)
+decimal bankAccountBalance; //Much more accurate than double, stores numbers rounded to 28 positions
+
+//bankAccountBalance = 2.34; compiler will convert variable into a double, must declare it with an M at the end of the declaration
+bankAccountBalance = 2.34M;
+
+Console.WriteLine(bankAccountBalance);
+
