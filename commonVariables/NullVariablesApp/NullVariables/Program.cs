0fab816b3f11e5058cfbdcc8eb0f664fe4c4e624
EddEnt <61289536+EddEnt@users.noreply.github.com>
2023-07-17 15:36:26 -0400

    Bool & Null commit

---
+++ commonVariables/NullVariablesApp/NullVariables/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+//Null - Lack of value
+
+//Have not asked a user for their age yet
+
+//int age = null; Can not convert null into an integer, must have a value
+
+int ? age = null; //? allows for a variable to be of a nullable value
+bool ? birthday = null;
+double ? weight = null;
+//string firstName = null; //Strings accept null being declared without the ?,
+                         //error message will still be displayed, because you
+                         //have not declare to the compiler that you may expect
+                         //some null value
+string ? firstName = null; //<--Better practice
+
+Console.WriteLine("Age: " + age);
+Console.WriteLine("Birthday: " + birthday);
+Console.WriteLine("Weight: " + weight);
+Console.WriteLine("First Name: " + firstName);
+Console.WriteLine("-------------------");
+
