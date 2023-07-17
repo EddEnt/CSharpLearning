c66248040487adc7b0a017b89f96b2f1fb54c4be
EddEnt <61289536+EddEnt@users.noreply.github.com>
2023-07-17 16:05:32 -0400

    Variables homework from C# Mastercourse

---
+++ commonVariables/VariablesWorkApp/VariablesWork/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+string name = "Edward Entecott";
+int age = 28;
+bool alive = true;
+string phoneNumber = "(123) 456-7890";
+
+Console.WriteLine("Welcome");
+Console.WriteLine("Is the information below correct?");
+Console.WriteLine("Name: " + name);
+Console.WriteLine("Age: " + age);
+Console.WriteLine("Alive?: " + alive);
+Console.WriteLine("Phone Number: " + phoneNumber);
+
+
