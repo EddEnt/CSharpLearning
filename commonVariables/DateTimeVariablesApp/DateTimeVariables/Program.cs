d02744d4253313d9c4db29974e81decd72dc9186
EddEnt <61289536+EddEnt@users.noreply.github.com>
2023-08-01 15:26:31 -0400

    DateTime Variables commit

---
+++ commonVariables/DateTimeVariablesApp/DateTimeVariables/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+using System.Globalization;
+
+DateTime today = DateTime.Now; //Use computer local time
+DateTime utcToday = DateTime.UtcNow; //UTC no offset
+
+DateTime birthday = DateTime.ParseExact("14/09/1994", "d/M/yyyy", CultureInfo.InvariantCulture); //Culture info requires adding System.Globalization,
+                                                                                                 //Invariant culture tells the system to disregard culture
+
+Console.WriteLine(today.ToString("F"));
+Console.WriteLine(birthday);
+Console.WriteLine(today.ToString("MMMM, dd, yyyy, H:m tt zzz"));
+Console.WriteLine(utcToday.ToString("MMMM, dd, yyyy, H:m tt zzz"));
