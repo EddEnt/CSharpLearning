c7c2cd7350d6e4cc3a66db53680613ba0aaf4fa9
EddEnt <61289536+EddEnt@users.noreply.github.com>
2023-08-01 15:56:03 -0400

    TimeOnly Variables

---
+++ commonVariables/TimeOnlyApp/TimeOnly/Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+TimeOnly opening = TimeOnly.Parse("8:00 AM");
+TimeOnly rightNow = TimeOnly.FromDateTime(DateTime.Now);
+TimeOnly holiday = TimeOnly.Parse("5:00 PM");
+
+Console.WriteLine(opening);
+Console.WriteLine(rightNow);
+Console.WriteLine(holiday);
+Console.WriteLine($"We are opening at {opening}, but will close at {holiday}.");
