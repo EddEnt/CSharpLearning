87136140ccaf02ff09b2a6b32154859d2703f296
EddEnt <61289536+EddEnt@users.noreply.github.com>
2023-08-02 15:35:33 -0400

    IfStatements upload

---
+++ conditionals/IfStatmentsApp/IfStatments/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+//bool isComplete = false;
+
+//if (isComplete)
+//{
+//    Console.WriteLine("The statement was true");
+//}
+//else
+//{
+//    Console.WriteLine("The statement was false");
+//}
+
+string? firstName;
+
+Console.Write("What is your first name?: "); ;
+firstName = Console.ReadLine();
+
+if (firstName.ToLower() == "edward")
+{
+    Console.WriteLine("Hello primary user!");
+}
+else
+{
+    Console.WriteLine($"Hello other user, {firstName}!");
+}
+
+Console.WriteLine("Program ending...");
