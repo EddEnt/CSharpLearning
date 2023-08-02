c95f6694b7c1af5f681daad934f8b0a7dfc98f7b
EddEnt <61289536+EddEnt@users.noreply.github.com>
2023-08-02 12:59:05 -0400

    Type Conversions Homework

---
+++ homework/typeConversionsHomeworkApp/typeConversionsHomework/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+string? userAgeText;
+int userAge;
+bool userAgeValid;
+
+Console.Write("Please enter your age: ");
+userAgeText = Console.ReadLine();
+
+userAgeValid = int.TryParse(userAgeText, out userAge);
+Console.WriteLine(userAge);
+Console.WriteLine($"You will be {userAge + 25} in 25 years time.");
+Console.WriteLine($"25 years ago, you were {userAge - 25}.");
+
