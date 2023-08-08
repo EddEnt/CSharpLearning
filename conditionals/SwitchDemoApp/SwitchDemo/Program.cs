b3ba16ab90ec2ac6d01b005d3201a2dd63d8ce2f
EddEnt <61289536+EddEnt@users.noreply.github.com>
2023-08-07 21:02:41 -0400

    Switch statment upload

---
+++ conditionals/SwitchDemoApp/SwitchDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+string firstName = "Edward";
+int age = 43;
+
+switch (age)
+{
+    case >= 0 and < 18:
+        Console.WriteLine("Child account detected.");
+        break;
+    case >= 18 and < 65:
+        Console.WriteLine("Regular account detected");
+        break;
+    case >= 65:
+        Console.WriteLine("Senior account detected");
+        break;
+    default:
+        Console.WriteLine("Age was not in expected range!");
+        break;
+}
+
+//switch (firstName.ToLower())
+//{
+//    //case "sue": If no code with a break, will fall through to the next case
+//    case "edward" or "sue":
+//        Console.WriteLine("Hello User");
+//        break;
+//    case "tom":
+//        Console.WriteLine("Oh, it's you...");
+//        break;
+//    default:
+//        Console.WriteLine("Unknown User...");
+//        break;
+//}
+
