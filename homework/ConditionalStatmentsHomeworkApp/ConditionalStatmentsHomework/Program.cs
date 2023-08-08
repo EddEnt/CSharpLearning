f3309004adb55c97031cab88ef7214eb6926904b
EddEnt <61289536+EddEnt@users.noreply.github.com>
2023-08-07 21:16:33 -0400

    Conditional Statements homework.

---
+++ homework/ConditionalStatmentsHomeworkApp/ConditionalStatmentsHomework/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+string? username;
+
+Console.Write("What is your name?: ");
+username = Console.ReadLine();
+
+switch (username.ToLower())
+{
+    case "edward":
+        Console.WriteLine("Hello, Edward. Welcome back to your training. ");
+        break;
+    case "tim":
+        Console.WriteLine("Hello, Professor. Welcome back.");
+        break;
+    case "":
+        Console.WriteLine("Empty entry. Please try again");
+        break;
+    default:
+        Console.WriteLine("Hello, Student. Welcome back to your training.");
+        break;
+}
+
