ab17fb520dbe59fd7ee3173dba110c6b16b4a15b
EddEnt <61289536+EddEnt@users.noreply.github.com>
2023-08-11 15:30:19 -0400

    Do/While Loops upload

---
+++ loops&Sets/DoLoopApp/DoLoop/Program.cs
@@ -0,0 +1,35 @@
+﻿
+bool isValidAge;
+int age;
+
+do
+{
+    Console.WriteLine("If you do not wish to enter your age, type 'exit' to quit...");
+    Console.Write("What is your age?: ");
+    string ageText = Console.ReadLine();
+
+	isValidAge = int.TryParse(ageText, out age);
+
+    if (ageText.ToLower() == "exit")
+    {
+        Environment.Exit(0);
+    }
+    if (isValidAge == false)
+	{
+		Console.WriteLine("Invalid age!");
+	}
+
+
+
+} while (isValidAge == false);
+
+do
+{
+    //Will run the code at least once!
+} while (true);
+
+while (true)
+{
+    //Will run the code zero or more times!
+}
+
