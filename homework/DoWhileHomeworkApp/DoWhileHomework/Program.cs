ee1a6b9553b160ac665c9efb20030307340cd4b6
EddEnt <61289536+EddEnt@users.noreply.github.com>
2023-08-11 15:48:50 -0400

    Do-While Homework upload

---
+++ homework/DoWhileHomeworkApp/DoWhileHomework/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+string username;
+bool programExitCommand = false;
+
+do
+{
+    Console.Write("Enter your name. To quit out of the program, type 'exit': ");
+    username = Console.ReadLine();
+
+    if (username.ToLower() == "tim")
+    {
+        Console.WriteLine($"Hello Professor {username}");
+
+    }
+
+    if (username.ToLower() != "tim" && username.ToLower() != "exit")
+    {
+        Console.WriteLine("Hello, Student.");
+    }
+
+    if (username.ToLower() == "exit")
+    {
+        //Environment.Exit(0); 
+        Console.WriteLine("Exiting...");
+        programExitCommand = true;
+    }
+
+
+} while (programExitCommand == false);
+
