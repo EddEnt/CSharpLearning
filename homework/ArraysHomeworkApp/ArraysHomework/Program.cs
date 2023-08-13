2fe846e3a5bc80473391b9e1f1b2da556eb4637d
EddEnt <61289536+EddEnt@users.noreply.github.com>
2023-08-13 10:46:59 -0400

    ArraysHomework upload

---
+++ homework/ArraysHomeworkApp/ArraysHomework/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+string[] names = new string[3];
+string inputUserNumber;
+int userNumber;
+
+names[0] = "John Doe";
+names[1] = "Jane Doe";
+names[2] = "Smithy Smith";
+
+int index = 0;
+bool isValidInput = false;
+
+while (!isValidInput) //!isValidInput means "as long as the input is not valid", will keep looping until it becomes True
+{
+    Console.Write("Enter a number between 1 and 3: ");
+    isValidInput = int.TryParse(Console.ReadLine(), out index);
+
+    if (!isValidInput || index < 1 || index > 3)
+    {
+        Console.WriteLine("Invalid input. Please enter a number between 1 and 3.");
+        isValidInput = false; // Continue the loop
+    }
+}
+
+string selectedName = names[index - 1];
+//Console.WriteLine($"The name at index {index} is: {selectedName}");
+Console.WriteLine($"You have selected {selectedName}, which is found at position {index - 1} in our array.");
+
+
---
+++ loops&Sets/ArraysApp/Arrays/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+//string [] firstNames = new string[5];
+
+//firstNames[0] = "Tim";
+//firstNames[1] = "Edward";
+//firstNames[2] = "Nick";
+//firstNames[3] = "Matt";
+//firstNames[4] = "Peter";
+
+//Console.WriteLine($"The names in the array are {firstNames[0]}, {firstNames[1]}, {firstNames[2]}, " +
+//                  $"{firstNames[3]}, {firstNames[4]}.");
+
+//Single quotes identifies a single char(character) More efficient in memory
+//Double quotes identifies a string of characters
+
+string data = "Edward, Nick, Alison, Penny, Peter";
+string [] firstNames = data.Split(',');
+string [] lastNames = new string[] { "En", "Te", "Co", "Tt", "Yy" };
+int [] age = new int[] { 2, 3, 4, 5, 6 };
+
+Console.WriteLine(firstNames.Length);
+Console.WriteLine(firstNames[1]);
+Console.WriteLine(lastNames.Length);
+Console.WriteLine(age.Length);
