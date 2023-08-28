00c3559136e423e73c22e61e055f61d1d579b0ba
EddEnt <61289536+EddEnt@users.noreply.github.com>
2023-08-28 13:50:18 -0400

    Dictionary Homework upload

--- homework/DictionaryHomeworkApp/DictionaryHomework/Program.cs
+++ homework/DictionaryHomeworkApp/DictionaryHomework/Program.cs
@@ -1,4 +1,27 @@
 ﻿
+int inputEmployeeKey;
 
-Dictionary<int, string> = new Dictionary<int, string>();
+Dictionary<int, string> employees = new Dictionary<int, string>();
+
+employees[1923] = "Edward";
+employees[5021] = "Mark";
+employees[1234] = "Juliet";
+employees[9217] = "Romeo";
+
+Console.WriteLine("--- Employee Records ---");
+Console.Write("Please enter the employee number you wish to find: ");
+
+inputEmployeeKey = Convert.ToInt32(Console.ReadLine());
+
+if (employees.ContainsKey(inputEmployeeKey))
+{
+    string resultEmployeeName = employees[inputEmployeeKey];
+    Console.WriteLine($"Result: {resultEmployeeName}");
+}
+else
+{
+    Console.WriteLine("Entry not found...");
+}
+
+
 
