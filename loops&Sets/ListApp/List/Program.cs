f9ea9da759f1ad55c7928df7f73ec33abd07a1fd
EddEnt <61289536+EddEnt@users.noreply.github.com>
2023-08-28 11:46:13 -0400

    List upload

---
+++ loops&Sets/ListApp/List/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+//List<T> - T = generic
+
+List<string> firstNames = new List<string>();   //A list of strings, can only hold strings. List<of type string>
+
+firstNames.Add("Ed");
+firstNames.Add("Tim");
+firstNames.Add("Nick");
+firstNames.Add("Sue");
+firstNames.Add("Bob");
+
+Console.WriteLine(firstNames[0]);
+Console.WriteLine(firstNames[2]);
+Console.WriteLine(firstNames.Count); //Returns the number of elements in the List
+Console.WriteLine(firstNames[firstNames.Count - 1]); //Counts through the list until the end, and returns the final entry
+
+List<int> ages = new List<int>();
+ages.Add(1);
+ages.Add(2);
+ages.Add(3);
+
+
