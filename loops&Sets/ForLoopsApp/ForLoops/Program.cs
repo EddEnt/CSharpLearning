553e4c8ab08f980660842b13da9ee2032c30e8da
EddEnt <61289536+EddEnt@users.noreply.github.com>
2023-08-28 14:24:10 -0400

    ForLoop upload

--- loops&Sets/ForLoopsApp/ForLoops/Program.cs
+++ loops&Sets/ForLoopsApp/ForLoops/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+//for (int i = 0; i < 10; i++)
+//{
+//    Console.WriteLine($"The value of i is {i}");
+//}
+
+//string data = "Tim,Sue,Bob,Jane,Frank";
+
+//List<string> firstNames = data.Split(',').ToList();
+
+//for (int i = 0; i < firstNames.Count; i++)
+//{
+//    Console.WriteLine($"{firstNames[i]} is present");
+//}
+
+
+List<decimal> charges = new(); //Shortcut to saying List<decimal> charges = new List<decimal>();
+
+charges.Add(23.78M);
+charges.Add(15.89M);
+charges.Add(125.00M);
+
+decimal total = 0;
+
+for (int i = 0; i < charges.Count; i++)
+{
+    total += charges[i];
+}
+
+Console.WriteLine($"Total charge is {total}");
+
