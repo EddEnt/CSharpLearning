bee690b4dc28d886ee5d751e80257ccac45ad058
EddEnt <61289536+EddEnt@users.noreply.github.com>
2023-08-28 16:19:02 -0400

    For Loops Homework

---
+++ homework/ForLoopsHomeworkApp/ForLoopsHomework/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+string inputFirstNames;
+
+List<string> firstNames = new();
+
+Console.WriteLine("--- First Names List ---");
+Console.WriteLine("Please enter first names into a List seperated by commas. " +
+                  "Do not add any spaces around the commas");
+
+inputFirstNames = Console.ReadLine();
+firstNames = inputFirstNames.Split(',').ToList();
+for (int i = 0; i < firstNames.Count; i++)
+{
+    Console.WriteLine($"Hello, {firstNames[i]}");
+}
+
+
