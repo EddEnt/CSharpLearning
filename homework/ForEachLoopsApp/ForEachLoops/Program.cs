37380b572c550cc79e9bd07260de5690baeb625c
EddEnt <61289536+EddEnt@users.noreply.github.com>
2023-08-28 22:19:08 -0400

    Foreach work upload

---
+++ homework/ForEachLoopsApp/ForEachLoops/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+string data = "Tim,Sue,Bob,Jane";
+List<string> firstNames = data.Split(',').ToList();
+
+//Foreach will return entries inside of a list/array
+//For allows fine tuning your return, until the bool expression evaluates to true
+foreach (string firstName in firstNames) //Can use var to work with a foreach, will be assigned the variable type based on the first entry in the list
+{
+    Console.WriteLine(firstName);
+}
