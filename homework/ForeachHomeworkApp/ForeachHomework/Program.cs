814754911f343f561fa71d15b5fe163e1b2d00f0
EddEnt <61289536+EddEnt@users.noreply.github.com>
2023-08-28 22:33:07 -0400

    Foreach Loop Homework upload

---
+++ homework/ForeachHomeworkApp/ForeachHomework/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+string inputFirstName;
+bool inputFinished = false;
+List<string> firstNames = new();
+
+Console.WriteLine("--- Foreach Loops ---");
+Console.WriteLine("Please enter first names. Once done, type 'exit'.");
+
+while (!inputFinished)
+{
+    Console.Write("First Name: ");
+    inputFirstName = Console.ReadLine();
+
+    if (inputFirstName.ToLower() == "exit")
+    {
+        break;
+    }
+
+    firstNames.Add(inputFirstName);
+
+}
+
+foreach (string firstName in firstNames)
+{
+    Console.WriteLine($"Hello, {firstName}");
+}
+
