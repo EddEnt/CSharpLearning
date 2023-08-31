0339400bf6b98df92fad6156b16e9bf585e6d1c5
EddEnt <61289536+EddEnt@users.noreply.github.com>
2023-08-31 16:52:26 -0400

    Methods work upload

--- methods/MethodsApp/Methods/Program.cs
+++ methods/MethodsApp/Methods/Program.cs
@@ -1,4 +1,38 @@
-﻿
+﻿//using static Methods.SampleMethods; NOT A GOOD PRACTICE. Always aim for call from a class with the class name for readability
+//                                    For situations, may be good, but can lead to ambigious calls where you will not be able
+//                                    to easily debug or read the program.
+using Methods; //Correct way
 
+//DRY - Don't Repeat Yourself
 
+//Not about completly eliminating repetition, but eliminating repeating
+//Better to have an issue in one area of your code, which will be fixed once, rather than
+//code that performs the same task, but split up into multiple classes or methods. That will require 
+//multiple fixes to it, which may be missed during debugging.
+
+//Example: Tax software. A calculation is performed, and the tax return is off by .1%. Logic is corrected, but
+//due to 2 similar methods being used, one is fixed and the other is not. Now the tax return is only off by .05%.
+//Better than it was, but still incorrect. Could lead to legal issues and fines for the company because of poor logic.
+
+//SOLID ( Mnemonic acronym for five design principles)
+//S - SRP (Single Responsibility Principle) - Every Class should have only one responsibility. Never does more than it should.
+
+//O - OCP (Open-Closed Principle)           - Software entites should be open for extension, but closed for modification. 
+//                                             Like a Parent-Child relationship, parent class is generalized, child is more specialized.
+
+//L - LSP (Liskov Substitution Principle)   - Functions that use pointers or references to base classes, must be able use
+//                                             objects of derived classes without knowing it; that is, if S is a subtype of T,
+//                                             then objects of type T in a program may be replaced with objects of type S
+//                                             without altering any of the desirable properties of that program.
+
+//I - ISP (Interface Segregation Principle) - No code should be forced to depend on methods it does not use. Intended to keep a system
+//                                             decoupled and easier to refactor, change and redeploy.
+
+//D - DIP (Dependency Inversion Principle)  - High level modules should not depend on low level modules; both should depend on abstractions.
+//                                             Abstractions should not depend on details. Details should depend on abstractions. Everything is
+//                                             loosely coupled together, allowing for greater maintainability. When tightly coupled, becomes
+//                                             much harder to fix or refactor.
+
+SampleConsoleMethods.SayHello();
+SampleConsoleMethods.SayGoodbye();
 
---
+++ methods/MethodsApp/Methods/SampleConsoleMethods.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+
+    //https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/access-modifiers
+    //Public - May be accessed by any other code in the same assembly or another assembly that references it.
+    //         The accessibility level of public members of a type is controlled by the accessibility level of the type itself
+    //         In other words, available from within the project and other projects that reference the prior project.
+
+    //Private - The type or member can be accessed only by code in the same class or struct. Only available from inside the class.
+
+    //Protected - The type or member can be accessed only by code in the same class, or in a class that is derived from that class.
+    //            Accessible from containting classes and derived classes. Not accessible from outside the class hierarchy, even in the same assembly.
+
+    //Internal - Types or members can be accessed from code that is part of the same compilation (Available only within the project).
+
+    //Protected Internal - Combination of Protected and Internal. The type or member can be accessed by any code in the assembly in which it's declared,
+    //                     or from within a derived class in another assembly.
+
+    //Private Internal - Combination of Private and Internal. Can be accessed by types derived from the class that are declared within its containing assembly.
+    //                   A way to provide access to a member onlt to specific derived classes within that same assembly.
+
+    //Static Class - Can not be instantiated. In other words, cannot use the "new" operator to create a variable of the class type.
+    //               Must call a method from within the class. Can be used as a convenient container for sets of methods that just operate on input parameters,
+    //               and do not have to get or set any internal instance fields.
+    
+    //Void - Return type is null. Method will not return a value. 
+
+    public static class SampleConsoleMethods //Always PascalCase when naming methods and classes (Captial letters at the start and for new words)
+    {
+        public static void SayHello()
+        {
+            Console.WriteLine("Hello from SampleConsoleMethods.cs");
+        }
+
+        public static void SayGoodbye() 
+        {
+            Console.WriteLine("Goodbye from SampleConsoleMethods.cs");
+        }
+    }
+}
