80380f55380b7cb988070eae5c35c4e0d2094ae2
EddEnt <61289536+EddEnt@users.noreply.github.com>
2023-09-07 20:30:47 -0400

    Basic Numeric Operations work

---
+++ commonVariables/BasicNumericOperationsApp/BasicNumericOperations/Program.cs
@@ -0,0 +1,21 @@
+﻿
+//Addition
+int firstNumber = 12;
+int secondNumber = 7;
+Console.WriteLine(firstNumber + secondNumber);
+
+//Mixing data stypes to force implicit type conversions
+string firstName = "Edward";
+int unitsSold = 7;
+Console.WriteLine($"{firstName} sold {unitsSold} units last week.");
+
+//Attempting Addition and Concatenation
+Console.WriteLine(firstName + " sold " + unitsSold + 7 + " units last week.");
+//Does not work. Compiler treats everything as a concatenation 
+
+//Parentheses to perform addition
+Console.WriteLine(firstName + " sold " + (unitsSold + 7) + " units last week.");
+//Parentheses becomes another overloaded operator. Parentheses form the "order of operations" operator.
+//Parentheses is calculated first, then concatenated
+//Avoid in practical code! Point is to help with understanding operators and operands as the compiler does
+
