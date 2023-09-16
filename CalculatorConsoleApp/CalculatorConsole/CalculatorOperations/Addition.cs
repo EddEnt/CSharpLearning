9e575e4e700ea40bddffd2645fbc03795f5f996a
Edward Entecott <61289536+EddEnt@users.noreply.github.com>
2023-09-16 12:53:29 -0400

    Calculator Operations folder and operation classes

---
+++ CalculatorConsoleApp/CalculatorConsole/CalculatorOperations/Addition.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorConsole.CalculatorOperations
+{
+    public class Addition
+    {
+        public static double AddNumbers(double number1, double number2 )
+        {
+            double calculatedResult = number1 + number2;
+            return calculatedResult;
+        }
+    }
+}
---
+++ CalculatorConsoleApp/CalculatorConsole/CalculatorOperations/Division.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorConsole.CalculatorOperations
+{
+    public class Division
+    {
+        public static double DivideNumbers(double number1, double number2)
+        {
+            double calculatedResult = double.NaN;
+            if (number2 != 0)
+            {
+                calculatedResult = number1 / number2;
+            }
+            return calculatedResult;
+        }
+    }
+}
