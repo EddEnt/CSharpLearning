e19ba38917fcc60f644e64adb2c24cea0312da16
EddEnt <61289536+EddEnt@users.noreply.github.com>
2023-09-20 14:26:35 -0400

    Instantiated Classes upload. Homework included in the same file, print names along with input addresses from seperate lists.

---
+++ InstantiatedClassesApp/InstantiatedClasses/ProcessAddress.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstantiatedClasses
+{
+    public class ProcessAddress
+    {
+        public static void ShowAddress(AddressModel address)
+        {
+            Console.WriteLine($"You live at {address.Street}, {address.City}, {address.Country} {address.PostalCode}");
+        }
+    }
+}
---
+++ InstantiatedClassesApp/InstantiatedClasses/Program.cs
@@ -0,0 +1,55 @@
+﻿
+
+using InstantiatedClasses;
+
+List<PersonModel> people = new List<PersonModel>();
+List<AddressModel> addresses = new List<AddressModel>();
+
+string firstName = string.Empty;
+string lastName = string.Empty;
+string street = string.Empty;
+string city = string.Empty;
+string country = string.Empty;
+string postalCode = string.Empty;
+
+do
+{
+    Console.Write("Enter your first name (or type 'exit' to stop): ");
+    firstName = Console.ReadLine();
+    Console.Write("Enter your last name: ");
+    lastName = Console.ReadLine();
+    Console.Write("Enter your street address: ");
+    street = Console.ReadLine();
+    Console.Write("Enter your city: ");
+    city = Console.ReadLine();
+    Console.Write("Enter your country: ");
+    country = Console.ReadLine();
+    Console.Write("Enter your postal code: ");
+    postalCode = Console.ReadLine();
+
+    if (firstName.ToLower() != "exit")
+    {
+        PersonModel person = new PersonModel();
+        person.FirstName = firstName;
+        person.LastName = lastName;
+        people.Add(person);
+        AddressModel address = new AddressModel();
+        address.Street = street;
+        address.City = city;
+        address.Country = country;
+        address.PostalCode = postalCode;
+        addresses.Add(address);
+    }
+} while (firstName.ToLower() != "exit");
+
+//foreach (PersonModel person in people)
+//{
+//    ProcessPerson.GreetPerson(person);
+//}
+
+for (int i = 0; i < Math.Min(people.Count, addresses.Count); i++)
+{
+    ProcessPerson.GreetPerson(people[i]);
+    ProcessAddress.ShowAddress(addresses[i]);
+}
+
