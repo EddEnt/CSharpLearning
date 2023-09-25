3fc8907b46ad51e64664b55a877c63aacdbd1cc6
EddEnt <61289536+EddEnt@users.noreply.github.com>
2023-09-24 22:10:43 -0400

    Upload of Property Types

---
+++ objectOrientedProgramming/PropertyTypesApp/PropertyTypes/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+using PropertyTypes.Models;
+
+PersonModel person = new PersonModel();
+
+person.FirstName = "Edward";
+person.LastName = "Dave";
+person.Age = 30;
+person.SIN = "123-456-789";
+
+Console.WriteLine(person.FirstName);
+Console.WriteLine(person.LastName);
+Console.WriteLine(person.Age);
+Console.WriteLine(person.SIN);
+
