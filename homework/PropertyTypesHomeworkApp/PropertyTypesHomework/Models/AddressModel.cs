a47b18fe177dc48078a1fb23709118cbe1fb31b7
EddEnt <61289536+EddEnt@users.noreply.github.com>
2023-09-24 22:31:02 -0400

    Upload of Property Types Homework

---
+++ homework/PropertyTypesHomeworkApp/PropertyTypesHomework/Models/AddressModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyTypesHomework.Models
+{
+    public class AddressModel
+    {
+        public string StreetAddress { private get; set; }
+        public string City { private get; set; }
+        public string Region { private get; set; }
+        public string Country { private get; set; }
+        public string PostalCode { private get; set; }
+        public string FullAddress { 
+            get
+            {
+                return $"{ StreetAddress }, { City }, { Region }, { Country }, { PostalCode }";
+            } 
+        }
+
+        public AddressModel(string streetAddress, string city, string region, string country, string postalCode)
+        {
+            StreetAddress = streetAddress ;
+            City = city ;
+            Region = region ;
+            Country = country ;
+            PostalCode = postalCode ;
+        }
+
+    }
+}
