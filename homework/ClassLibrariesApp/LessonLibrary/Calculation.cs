1d04d732d45cdc46ad9189db6dec2bdfa71c6946
EddEnt <61289536+EddEnt@users.noreply.github.com>
2023-09-25 15:05:27 -0400

    Class Libraries and homework

---
+++ homework/ClassLibrariesApp/LessonLibrary/Calculation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LessonLibrary
+{
+    public static class Calculation
+    {
+        public static double Addition(double x, double y) 
+        {
+            double outputAddition = 0;
+            outputAddition = x + y;
+
+            return outputAddition;
+        }
+    }
+}
