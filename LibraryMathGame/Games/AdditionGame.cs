d229433f5c9905c704e67f06030b9682f5290a92
EddEnt <61289536+EddEnt@users.noreply.github.com>
2023-10-21 15:12:45 -0400

    Removed commented code.

--- LibraryMathGame/Games/AdditionGame.cs
+++ LibraryMathGame/Games/AdditionGame.cs
@@ -50,45 +50,4 @@ namespace LibraryMathGame.Games
     }
 }
 
-    //private static int PlayGame(int min, int max, int numberOfQuestionsToPlay, string gameType)
-    //{
-    //    int numberOfQuestions = 0;
-    //    int correctAnswers = 0;
-
-    //    while (numberOfQuestions < numberOfQuestionsToPlay)
-    //    {
-    //        int num1 = new Random().Next(min, max + 1);
-    //        int num2 = new Random().Next(min, max + 1);
-
-    //        Console.Write($"What is {num1} + {num2}? ");
-    //        string inputUserAnswer = Console.ReadLine().Trim().ToLower();
-
-    //        if (inputUserAnswer == "q")
-    //        {
-    //            Console.WriteLine($"Exiting the {gameType} game.");
-    //            return correctAnswers;
-    //        }
-
-    //        if (int.TryParse(inputUserAnswer, out int userAnswer) && userAnswer == num1 + num2)
-    //        {
-    //            Console.WriteLine("Correct answer!");
-    //            correctAnswers++;
-    //        }
-    //        else
-    //        {
-    //            Console.WriteLine($"Incorrect. The correct answer is {num1 + num2}.");
-    //        }
-
-    //        numberOfQuestions++;
-
-    //        if (numberOfQuestions % 5 == 0)
-    //        {
-    //            if (!ContinueGame.AskToContinueGame())
-    //                return correctAnswers; // Exit the game
-    //        }
-    //    }
-
-    //    return correctAnswers;
-    //}
-
 
