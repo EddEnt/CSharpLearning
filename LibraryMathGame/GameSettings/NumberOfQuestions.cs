3079cea2d5773b4e43e73a513b9b57454615d254
EddEnt <61289536+EddEnt@users.noreply.github.com>
2023-10-21 15:16:26 -0400

    Moved Difficulty.cs and NumberOfQuestions.cs to GameSettings folder.

--- LibraryMathGame/GameSettings/NumberOfQuestions.cs
+++ LibraryMathGame/GameSettings/NumberOfQuestions.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LibraryMathGame
+namespace LibraryMathGame.GameSettings
 {
     internal class NumberOfQuestions
     {
