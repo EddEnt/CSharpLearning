5c1e9c1fda613568098b9e249ed16d84db4d09e4
EddEnt <61289536+EddEnt@users.noreply.github.com>
2023-10-21 12:13:01 -0400

    Refactored code for readability. Creation of PlayGame blueprint for running games.

--- LibraryMathGame/Games/AdditionGame.cs
+++ LibraryMathGame/Games/AdditionGame.cs
@@ -17,7 +17,6 @@ namespace LibraryMathGame.Games
             while (true)
             {
                 int min, max;
-                int numberOfQuestions = 0;
                 int numberOfQuestionsToPlay = 0;
                 int correctAnswers = 0;
 
@@ -36,7 +35,7 @@ namespace LibraryMathGame.Games
                 // Set min and max int range based on difficulty
                 Difficulty.SetDifficultyRange(difficulty, out min, out max);
 
-                correctAnswers = PlayAdditionGame(min, max, numberOfQuestionsToPlay);
+                correctAnswers = PlayGame.RunPlayGame(min, max, numberOfQuestionsToPlay, "Addition");
 
                 // Record the game session
                 GameSessionManager.RecordGameSession("Addition", difficulty, numberOfQuestionsToPlay, correctAnswers);
@@ -44,48 +43,6 @@ namespace LibraryMathGame.Games
                 if (!ContinueGame.AskToContinueGame())
                     return; // Exit addition game
             }
-        }
-
-
-        private static int PlayAdditionGame(int min, int max, int numberOfQuestionsToPlay)
-        {
-            int numberOfQuestions = 0;
-            int correctAnswers = 0;
-
-            while (numberOfQuestions < numberOfQuestionsToPlay)
-            {
-                int num1 = new Random().Next(min, max + 1);
-                int num2 = new Random().Next(min, max + 1);
-
-                Console.Write($"What is {num1} + {num2}? ");
-                string inputUserAnswer = Console.ReadLine().Trim().ToLower();
-
-                if (inputUserAnswer == "q")
-                {
-                    Console.WriteLine("Exiting the addition game.");
-                    return correctAnswers;
-                }
-
-                if (int.TryParse(inputUserAnswer, out int userAnswer) && userAnswer == num1 + num2)
-                {
-                    Console.WriteLine("Correct answer!");
-                    correctAnswers++;
-                }
-                else
-                {
-                    Console.WriteLine($"Incorrect. The correct answer is {num1 + num2}.");
-                }
-
-                numberOfQuestions++;
-
-                if (numberOfQuestions % 5 == 0)
-                {
-                    if (!ContinueGame.AskToContinueGame())
-                        return correctAnswers; // Exit addition game
-                }
-            }
-
-            return correctAnswers;
         }
 
     }
---
+++ LibraryMathGame/PlayGame.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryMathGame
+{
+    internal class PlayGame
+    {
+        //Blueprint for Gametypes
+        internal static int RunPlayGame(int min, int max, int numberOfQuestionsToPlay, string gameType)
+        {
+            int numberOfQuestions = 0;
+            int correctAnswers = 0;
+
+            while (numberOfQuestions < numberOfQuestionsToPlay)
+            {
+                int num1 = new Random().Next(min, max + 1);
+                int num2 = new Random().Next(min, max + 1);
+
+                Console.Write($"What is {num1} + {num2}? ");
+                string inputUserAnswer = Console.ReadLine().Trim().ToLower();
+
+                if (inputUserAnswer == "q")
+                {
+                    Console.WriteLine($"Exiting the {gameType} game.");
+                    return correctAnswers;
+                }
+
+                if (int.TryParse(inputUserAnswer, out int userAnswer) && userAnswer == num1 + num2)
+                {
+                    Console.WriteLine("Correct answer!");
+                    correctAnswers++;
+                }
+                else
+                {
+                    Console.WriteLine($"Incorrect. The correct answer is {num1 + num2}.");
+                }
+
+                numberOfQuestions++;
+
+                if (numberOfQuestions % 5 == 0)
+                {
+                    if (!ContinueGame.AskToContinueGame())
+                        // Exit the game
+                        return correctAnswers;
+                }
+            }
+
+            return correctAnswers;
+        }
+    }
+}
