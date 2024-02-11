61860db3826861f240a0f42c2d1244960d1ca3ec
EddEnt <61289536+EddEnt@users.noreply.github.com>
2024-02-11 14:13:26 -0500

    TempCommit

---
+++ CsvHelper/Program.cs
@@ -0,0 +1,5 @@
+﻿using CsvHelper;
+using CsvHelper.SeedData;
+
+SeedMethods seedMethods = new SeedMethods();
+seedMethods.GenerateEntries();
---
+++ CsvHelper/SeedData/SeedMethods.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvHelper.SeedData
+{
+    internal class SeedMethods
+    {
+        public static void GenerateEntries()
+        {
+            string filePath = "entries.csv";
+            Random random = new Random();
+
+            if (!File.Exists(filePath))
+            {
+                using (StreamWriter writer = new StreamWriter(filePath))
+                {
+                    for (int i = 0; i < 1000; i++)
+                    {
+                        int randomNumber = random.Next(1, 121);
+                        writer.WriteLine(randomNumber);
+                    }
+                }
+            }
+        }
+    }
+}
